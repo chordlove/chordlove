dff50c320f01ac5fd75946c2d1bc62ca12421ad9
Anders Nawroth <anders@neotechnology.com>
2013-10-25 23:24:15 +0200

    Added more metadata and a 600px icon.

--- index.php
+++ index.php
@@ -20,18 +20,38 @@ if ( $_SERVER['QUERY_STRING'] && $_SERVER['QUERY_STRING'] !== NULL && strlen($_S
 <html>
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
 <title>$output - Chordlove.com</title>
+
+<meta name=”description” content=”$output at Chordlove, the number one free online tool for sharing song chords and lyrics.”>
+
+<meta property=”og:title” content=”$output at Chordlove.com.”>
+<meta property=”og:type” content=”music.song”>
+<meta property=”og:image” content=”http://cdn.chordlove.com/images/icon-600.png”>
+<meta property="og:image:type" content="image/png">
+<meta property="og:image:width" content="600">
+<meta property="og:image:height" content="600">
+<meta property=”og:description” content=”$output at Chordlove.com, the number one free online tool for sharing song chords and lyrics.”>
+
+<meta name=”twitter:card” content=”summary”>
+<meta name="twitter:site" content="@ChordloveApp">
+<meta name=”twitter:title” content=”$output at Chordlove.com”>
+<meta name=”twitter:description” content=”$output at Chordlove.com, the number one free online tool for sharing song chords and lyrics.”>
+<meta name=”twitter:image” content=”http://cdn.chordlove.com/images/icon-600.png”>
+
 <script>
   window.location.replace( '/#!' + window.location.search.substring(1) );
 </script>
 </head>
-<body>
+<body itemscope itemtype="http://schema.org/Article">
   <header>
-    <img src="http://cdn.chordlove.com/images/icon-47.png" alt="">
-    <h1>$output</h1>
+    <h1 itemprop="name">$output</h1>
   </header>
   <div id="content">
-    Chordlove is a tool for sharing song chords and lyrics.
+    <p itemprop="description">
+    <img itemprop="image" src="http://cdn.chordlove.com/images/icon-600.png" alt="" style="width:3.75em;height:3.75em;">
+    Chordlove.com, the number one free online tool for sharing song chords and lyrics.
+    </p>
   </div>
 </body>
 </html>
